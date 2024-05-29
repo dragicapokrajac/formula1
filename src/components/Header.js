c01c6d5dc4920f59bc98743c7ea2db060348804e
DJukor <djukor@gmail.com>
2024-05-29 20:32:07 +0200

    Placeholder search bar.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
 import LinkLogo from "../img/F1-logo.png";
+import SearchBar from "./SearchBat";
 
 const Header = () => {
    return (
       <header className="header">
+         <SearchBar />
          <Link to="/">
             <li className="Logo">
                <img src={LinkLogo} alt="Logo"></img>
