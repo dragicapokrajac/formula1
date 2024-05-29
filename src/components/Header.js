51898753f9b891c92e8fffc710815fec829b4ee1
DJukor <djukor@gmail.com>
2024-05-29 14:53:04 +0200

    Header sa logo i home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import LinkLogo from "../img/F1-logo.png";
 
 const Header = () => {
     return (
         <div className="header">
             <h1>Header probni</h1>
-            <p>Text</p>
+            <Link to="/"><li className="Logo"><img src={LinkLogo} alt="Logo"></img>Logo</li></Link>
         </div>
     )
 }
