a19aa3e9c8aaee7451304b9700227e2ea11bb91f
DJukor <djukor@gmail.com>
2024-05-31 16:19:00 +0200

    dorada

--- src/components/Breadcrumbs.js
+++ src/components/Breadcrumbs.js
@@ -2,15 +2,14 @@ import { Link } from "react-router-dom";
 
 const Breadcrumbs = ({tabs}) => {
 
-    // const crumbs = [];
-
+    //Zakomentarisan Inline CSS za mogucu buducu uprotrebu
     return <div>
         <div>
             <ul> {tabs?.map((crumb, i) => {
                 return (
                     <ul>
                         <li key={i}>
-                            {/* {i === 0 && <img src={require("../img/icons/icons8-helmet-50.png")} style={{ maxWidth: 15 }} />} */}
+                            {/* {i === 0 && <img src={require("../img/icons/icons8-helmet-50.png")} style={{ width: 5, height: "auto" }} />} */}
                             {i < tabs.length - 1 ? (<Link to={crumb.path}>{crumb.name}</Link>) : (<span> {crumb.name} </span>)}
                         </li>
                     </ ul>
