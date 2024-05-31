21ee57b5a7e5ca82895a670b9ad5b4621ed6131b
DJukor <djukor@gmail.com>
2024-05-31 16:51:57 +0200

    Zakomentarisan Inline CSS za mogucu buducu uprotrebu

--- src/components/Breadcrumbs.js
+++ src/components/Breadcrumbs.js
@@ -1,15 +1,12 @@
 import { Link } from "react-router-dom";
 
 const Breadcrumbs = ({ tabs }) => {
-
-    //Zakomentarisan Inline CSS za mogucu buducu uprotrebu
     return <div>
         <div>
             <ul> {tabs?.map((crumb, i) => {
                 return (
                     <ul>
                         <li key={i}>
-                            {/* {i === 0 && <img src={require("../img/icons/icons8-helmet-50.png")} style={{ width: 5, height: "auto" }} />} */}
                             {i < tabs.length - 1 ? (<Link to={crumb.path}>{crumb.name}</Link>) : (<p> {crumb.name} </p>)}
                         </li>
                     </ ul>
@@ -22,3 +19,6 @@ const Breadcrumbs = ({ tabs }) => {
 }
 
 export default Breadcrumbs;
+
+//Zakomentarisan Inline CSS za mogucu buducu uprotrebu
+{/* {i === 0 && <img src={require("../img/icons/icons8-helmet-50.png")} style={{ width: 5, height: "auto" }} />} */}
