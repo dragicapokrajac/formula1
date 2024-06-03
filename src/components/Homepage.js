c3cc945bf26eede39510f8a1ed5e223152e457ee
DJukor <djukor@gmail.com>
2024-06-03 14:16:43 +0200

    Izabcena nepotrebna klasa u SCSS je preprvaljeno

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -4,13 +4,13 @@ const Homepage = () => {
    return (
       <section className="homepage-container">
          <div  className="block driver-block">
-            <NavLink to="/drivers" className="block">Drivers</NavLink>
+            <NavLink to="/drivers">Drivers</NavLink>
          </div>
          <div className="block team-block">
-            <NavLink to="/teams" className="block">Teams</NavLink>
+            <NavLink to="/teams" >Teams</NavLink>
          </div>
          <div className="block races-block">
-            <NavLink to="/races" className="block">Races</NavLink>
+            <NavLink to="/races" >Races</NavLink>
          </div>
       </section>
    );
