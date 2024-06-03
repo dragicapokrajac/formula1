ba46ee9fe1da82c1e2b2e8a498d03562f92d42af
DJukor <djukor@gmail.com>
2024-06-03 13:56:39 +0200

    Okvir oko teksta radi kao dugme

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -4,13 +4,13 @@ const Homepage = () => {
    return (
       <section className="homepage-container">
          <div  className="block driver-block">
-            <NavLink to="/drivers">Drivers</NavLink>
+            <NavLink to="/drivers" className="block">Drivers</NavLink>
          </div>
          <div className="block team-block">
-            <NavLink to="/teams">Teams</NavLink>
+            <NavLink to="/teams" className="block">Teams</NavLink>
          </div>
          <div className="block races-block">
-            <NavLink to="/races">Races</NavLink>
+            <NavLink to="/races" className="block">Races</NavLink>
          </div>
       </section>
    );
