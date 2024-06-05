2a96952dad6a3f20a12568cd7ccf88f625afc937
Mikilena <mikilenam@gmail.com>
2024-06-05 02:57:11 +0200

    searchBar stilizacija

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,17 +1,36 @@
+import React, { useEffect, useRef } from 'react';
 import searchIcon from '../img/icons/search_15px.png';
 
 const SearchBar = ({ type, placeholder, value, onChange }) => {
+   const inputRef = useRef(null);
+
+   useEffect(() => {
+      const searchBoxClick = () => {
+         inputRef.current.classList.toggle('expanded');
+      };
+      const searchBox = document.querySelector('.search-box');
+      searchBox.addEventListener('click', searchBoxClick);
+
+      return () => {
+         searchBox.removeEventListener('click', searchBoxClick);
+      };
+   }, []);
+
    return (
       <div className='search-wrapper'>
-         < div class="search-box" >
+         <div className="search-box">
             <input
+               ref={inputRef}
+               className="input-search"
                type={type}
                placeholder={placeholder}
                value={value}
                onChange={onChange}
             />
-            <img src={searchIcon} alt="search icon" />
-         </div >
+            <button>
+               <img src={searchIcon} alt="search icon" />
+            </button>
+         </div>
       </div>
    );
 };
